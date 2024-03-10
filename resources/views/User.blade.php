cb64c1a534f7bd4a837300e52a01ee7f871f2adb
Andika Jaya Nanda <andikajn25@gmail.com>
2024-03-10 17:34:28 +0700

    Praktikum 2.4 – Retreiving or Creating Models

--- resources/views/User.blade.php
+++ resources/views/User.blade.php
@@ -4,17 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Data User</title>
+    <title>Data User </title>
 </head>
-<body>
-    <h1>Data Level Pengguna</h1>
-    <table border="1" cellpadding="2" cellspacing="0">
-        <tr>
-            <th>Jumlah Pengguna</th>
-        </tr>
-        <tr>
-            <td>{{ $data }}</td>
-        </tr>
-    </table>
-</body>
+    <body>
+        <h1>Data Level Pengguna</h1>
+        <table border="1" cellpadding="2" cellspacing="0">
+            <tr>
+                <th>ID</th>
+                <th> Username</th>
+                <th>Nama</th>
+                <th>ID Level Pengguna</th>
+
+            </tr>
+            {{-- @foreach ($data as $d) --}}
+            <tr>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
+            </tr>
+            {{-- @endforeach --}}
+        </table>
+    </body>
 </html>
